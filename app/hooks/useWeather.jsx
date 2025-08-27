bd1ace9a9029e5574d520dbb0118bcc769c10d1e
user <renanmakoto@outlook.com>
2025-08-27 08:49:50 -0400

    Organize comments

--- app/hooks/useWeather.jsx
+++ app/hooks/useWeather.jsx
@@ -54,7 +54,7 @@ export default function useWeather() {
     let idx = hourlyTimes.indexOf(currentTimeStr)
     if (idx !== -1) return idx
 
-    // If exact not found, find the first time >= currentTimeStr
+    //If exact not found, find the first time >= currentTimeStr
     idx = hourlyTimes.findIndex(t => t >= currentTimeStr)
     if (idx !== -1) return idx
 
