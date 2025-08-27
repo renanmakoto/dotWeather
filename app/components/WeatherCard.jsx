a36b10c9af96a3cd90fb14d60ffb4d92909c24f5
user <renanmakoto@outlook.com>
2025-08-27 08:51:58 -0400

    Code adjustments

--- app/components/WeatherCard.jsx
+++ app/components/WeatherCard.jsx
@@ -68,7 +68,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold' 
   },
   icon: { 
-    marginVertical: 10 },
+    marginVertical: 10 
+  },
   currentTemp: { fontSize: 48, color: '#fff', fontWeight: 'bold' },
   currentDetails: { fontSize: 18, color: '#fff', marginVertical: 5 },
 
