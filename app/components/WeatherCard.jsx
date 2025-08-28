e9d297f3cdf717e091f3016a5a2a2a2b7576e5bb
user <renanmakoto@outlook.com>
2025-08-28 08:49:25 -0400

    Organize code

--- app/components/WeatherCard.jsx
+++ app/components/WeatherCard.jsx
@@ -77,7 +77,8 @@ const styles = StyleSheet.create({
   },
   currentDetails: { 
     fontSize: 18, 
-    color: '#fff', marginVertical: 5 },
+    color: '#fff', 
+    marginVertical: 5 },
 
   sectionTitle: { fontSize: 18, color: '#fff', fontWeight: '600', marginTop: 16, marginBottom: 8, alignSelf: 'flex-start' },
 
