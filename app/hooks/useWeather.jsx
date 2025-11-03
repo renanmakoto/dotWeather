6bd63b9e242d845a53f57eb49ce39c0d065334df
user <renanmakoto@outlook.com>
2025-11-03 08:36:43 -0500

    Clean code

--- app/hooks/useWeather.jsx
+++ app/hooks/useWeather.jsx
@@ -44,8 +44,6 @@ export default function useWeather() {
     }
   }
 
-  //Helpers
-
   //Find the index in hourly.time that is >= current time string
   function findCurrentHourIndex(hourlyTimes, currentTimeStr) {
     //Timestamps are like "2025-08-22T16:00" in the city's local time.
