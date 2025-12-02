021b67757ebfa82f75b3faf4f7ebb6077da14b6e
renanmakoto <renanmakoto@outlook.com>
2025-12-02 12:24:40 -0500

    Refactor code

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
+import { StatusBar, View } from 'react-native'
 import { Stack } from 'expo-router'
-import { StatusBar, View, Text } from 'react-native'
+
+const HEADER_HEIGHT = 40
+
+const headerStyle = {
+  backgroundColor: 'white',
+  height: HEADER_HEIGHT,
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  paddingBottom: 0,
+}
+
+function MinimalHeader() {
+  return <View style={headerStyle} />
+}
 
 export default function Layout() {
   return (
     <>
-      <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <Stack
         screenOptions={{
           headerTransparent: true,
           headerTitle: '',
-          header: () => (
-            <View
-              style={{
-                backgroundColor: 'white',
-                height: 40,
-                justifyContent: 'flex-end',
-                alignItems: 'center',
-                paddingBottom: 0,
-              }}
-            >
-            </View>
-          ),
+          header: MinimalHeader,
         }}
       />
     </>
