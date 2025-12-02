074ca041e2cc7e2b756126d86d5f90b93cd309ae
renanmakoto <renanmakoto@outlook.com>
2025-12-02 12:25:11 -0500

    Refactor code

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import WeatherScreen from './screens/WeatherScreen'
-import { useNavigation } from 'expo-router'
 
 export default function Index() {
-  const navigation = useNavigation()
-
-  // React.useEffect(() => {
-  //   navigation.setOptions({
-  //     title: 'dotWeather',
-  //     headerTitleAlign: 'center',
-  //   })
-  // }, [navigation])
-
   return <WeatherScreen />
 }
